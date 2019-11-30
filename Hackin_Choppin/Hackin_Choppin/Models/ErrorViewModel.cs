87acdf3a0f0aeccac133b0eb96fb3c1de8d8b17c
Jake <jake.grozdanovski.13@gmail.com>
2019-11-30 15:02:52 +1100

    Jake: initial code base

---
+++ Hackin_Choppin/Hackin_Choppin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hackin_Choppin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
